ccf17c9286dded9296fed8b7ca63d0bbd5a7524a
Lahari Challa <63194842+lahari-challa@users.noreply.github.com>
2025-10-29 17:08:44 -0400

    Update projectpart1.py

--- projectpart1.py
+++ projectpart1.py
@@ -4,7 +4,7 @@ import csv
 
 app = Flask(__name__)
 
-def getYourName(csv_file, file_name):
+def getYourNameFrom(csv_file, file_name):
     with open(csv_file,'r') as file:
         reader = csv.DictReader(file)
 
